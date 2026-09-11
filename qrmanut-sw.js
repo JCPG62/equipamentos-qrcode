880057e599d810960c19ac3754d7ab97b91410a6
JCPG62 <jotacpg@gmail.com>
2026-09-11 17:22:41 -0300

    Update qrmanut-sw.js

--- qrmanut-sw.js
+++ qrmanut-sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="qrmanut-static-7.6.6.24.12";
+const CACHE_NAME="qrmanut-static-7.6.6.24.12-r12-modalfix";
 const STATIC_ASSETS=[
   "./equip_formulario.html",
   "./manifest.webmanifest",
